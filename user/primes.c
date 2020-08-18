e7ae648eca2d25555d2eb2928d6ec0f511203cee
yuanyork <1055286585@qq.com>
2020-08-18 19:10:42 +0800

    prime

--- user/primes.c
+++ user/primes.c
@@ -1,19 +1,20 @@
 #include"kernel/types.h"
 #include"kernel/stat.h"
 #include"user/user.h"
+
+#define R 0
+#define W 1
+
+
 int main(void){
-    int p,rec;int fd[2];
+    int p,rec,nums[35];int fd[2];
     pipe(fd);
-    for(int i=2;i<=35;i++){
-        write(fd[1],i,4)
-    }
-    if()
-    read(fd[0],p,4);
-    fprintf("prime %d",p);
+    
     while(1){
-        read(fd[0],rec,4);
-        if(rec%p!=0){
-            write(fd[1],rec,4);
+        if(fork()==0){
+            
+        }else{
+
         }
     }
 
